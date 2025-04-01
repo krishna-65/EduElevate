0e16ef0440b63b2a4f6fdd0d4f52c353307ea22a
krishna-65 <sharmakrishnakant721@gmail.com>
2025-04-01 08:34:40 +0530

    ass

--- frontend/src/components/common/Logo.jsx
+++ frontend/src/components/common/Logo.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 const Logo = ({className})=>{
     return (
         <div className={`text-2xl font-bold bg-gradient-to-r bg-clip-text text-transparent from-[#6674CC] via-[purple] to-[orange] ${className}`}>
--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "https://eduelevate-myp8.onrender.com/api/v1"
+const BASE_URL = "https://eduelevate-8s1p.onrender.com/api/v1"
 //const  BASE_URL = "http://localhost:8000/api/v1"
 
 // AUTH ENDPOINTS
